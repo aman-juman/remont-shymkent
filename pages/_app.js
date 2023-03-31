af0117058964aba4fddff71c9a0f7aa5f17287cb
Aman Juman <alamanshogun@gmail.com>
2023-03-31 22:04:59 +0600

    version-1.06

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
 
   return(
       <>
-          <Script strategy="afterInteractive" async src="https://www.googletagmanager.com/gtag/js?id=G-SHL4Q6LC7B" />
+          <Script strategy="afterInteractive" async src="https://www.googletagmanager.com/gtag/js?id=G-C6CV9KSMQN" />
           <Script
               id='google-analytics'
               strategy="afterInteractive"
@@ -18,24 +18,23 @@ export default function App({ Component, pageProps }) {
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
 
-                  gtag('config', 'G-SHL4Q6LC7B');`
+                  gtag('config', 'G-C6CV9KSMQN');`
               }}
           />
           <Script strategy="afterInteractive"            id='Yandex-metrika'
                   dangerouslySetInnerHTML={{
                       __html: `
-                     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
     m[i].l=1*new Date();
     for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
     k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-    ym(92918445, "init", {
+    ym(93032452, "init", {
     clickmap:true,
     trackLinks:true,
     accurateTrackBounce:true,
     webvisor:true,
-    trackHash:true,
     ecommerce:"dataLayer"
 });
                       `
@@ -47,5 +46,6 @@ export default function App({ Component, pageProps }) {
       )
 
 }
+
 
 
